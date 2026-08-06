c33228124d6a46a063b2039f4d0557594ab5e98b
Sandmeyer <sandmx.work@gmail.com>
2026-08-06 20:45:33 +0800

    Use infallible conversion to teach From trait
    track: 同步上游 rustlings commit
    
    [Commit db5ad7f42f8b59dc8b2c7cf6ff26203d447a6f73]

--- solutions/23_conversions/conversions2.rs
+++ solutions/23_conversions/conversions2.rs
@@ -1,54 +1,36 @@
 // `From` 特征用于值到值的转换。如果实现了 `From`，那么会自动提供 `Into` 的实现。
 // 你可以在文档中阅读更多关于它的内容:
 // https://doc.rust-lang.org/std/convert/trait.From.html
+//
+// 仙女 Frank 想从地精 Grace 那里购买一些松露，Grace 是一位举世闻名的巧克力大师。
+// 松露以 GnomeCoin 计价，但 Frank 只有 FairyCredit。请通过提供 `From` 实现来
+// 帮助 Frank 将他的 FairyCredit 转换为 GnomeCoin。按照当前汇率，1 个 FairyCredit
+// 价值 100 个 GnomeCoin。
 
 #[derive(Debug)]
-struct Person {
-    name: String,
-    age: u8,
-}
+struct FairyCredit(u32);
 
-// 我们实现 `Default` 特征，以便在提供的字符串无法转换为 `Person` 对象时将其用作备用方案。
-impl Default for Person {
-    fn default() -> Self {
-        Self {
-            name: String::from("John"),
-            age: 30,
-        }
+#[derive(Debug, PartialEq)]
+struct GnomeCoin(u64);
+
+impl From<FairyCredit> for GnomeCoin {
+    fn from(value: FairyCredit) -> Self {
+        Self(value.0 as u64 * 100)
     }
 }
 
-impl From<&str> for Person {
-    fn from(s: &str) -> Self {
-        let mut split = s.split(',');
-        let (Some(name), Some(age), None) = (split.next(), split.next(), split.next()) else {
-            //                      ^^^^ 不应该有第三个元素
-            return Self::default();
-        };
-
-        if name.is_empty() {
-            return Self::default();
-        }
-
-        let Ok(age) = age.parse() else {
-            return Self::default();
-        };
-
-        Self {
-            name: name.into(),
-            age,
-        }
-    }
-}
+// 注意，我们不应该提供反向转换：从 GnomeCoin 转换为 FairyCredit。
+// 因为小于 100 的 GnomeCoin 无法表示为 FairyCredit，这会使转换有损。
+// `From` 特征只适用于无失败且无损的转换。
 
 fn main() {
     // 使用 `from` 函数。
-    let p1 = Person::from("Mark,20");
-    println!("{p1:?}");
+    let g1 = GnomeCoin::from(FairyCredit(12));
+    println!("{g1:?}");
 
-    // 由于 `Person` 实现了 `From`，我们也可以使用 `Into`。
-    let p2: Person = "Gerald,70".into();
-    println!("{p2:?}");
+    // 由于 `GnomeCoin` 实现了 `From`，我们也可以使用 `Into`。
+    let g2: GnomeCoin = FairyCredit(9).into();
+    println!("{g2:?}");
 }
 
 #[cfg(test)]
@@ -56,79 +38,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_default() {
-        let dp = Person::default();
-        assert_eq!(dp.name, "John");
-        assert_eq!(dp.age, 30);
+    fn test_from() {
+        let g = GnomeCoin::from(FairyCredit(12));
+        assert_eq!(g, GnomeCoin(1200));
     }
 
     #[test]
-    fn test_bad_convert() {
-        let p = Person::from("");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
-    }
-
-    #[test]
-    fn test_good_convert() {
-        let p = Person::from("Mark,20");
-        assert_eq!(p.name, "Mark");
-        assert_eq!(p.age, 20);
-    }
-
-    #[test]
-    fn test_bad_age() {
-        let p = Person::from("Mark,twenty");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
-    }
-
-    #[test]
-    fn test_missing_comma_and_age() {
-        let p: Person = Person::from("Mark");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
-    }
-
-    #[test]
-    fn test_missing_age() {
-        let p: Person = Person::from("Mark,");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
-    }
-
-    #[test]
-    fn test_missing_name() {
-        let p: Person = Person::from(",1");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
-    }
-
-    #[test]
-    fn test_missing_name_and_age() {
-        let p: Person = Person::from(",");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
-    }
-
-    #[test]
-    fn test_missing_name_and_invalid_age() {
-        let p: Person = Person::from(",one");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
-    }
-
-    #[test]
-    fn test_trailing_comma() {
-        let p: Person = Person::from("Mike,32,");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
-    }
-
-    #[test]
-    fn test_trailing_comma_and_some_string() {
-        let p: Person = Person::from("Mike,32,dog");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
+    fn test_into() {
+        let g: GnomeCoin = FairyCredit(9).into();
+        assert_eq!(g, GnomeCoin(900));
     }
 }

0383dd6e9eeb87f975f29fa8979ba0e0b6c20c6a
Sandmeyer <sandmx.work@gmail.com>
2026-08-06 20:49:24 +0800

    strings4: remove From-based conversion
    track: 同步上游 rustlings commit
    
    [Commit b18a8c3036c962d3a09bd04d0537ad86c046b3f7]

--- solutions/09_strings/strings4.rs
+++ solutions/09_strings/strings4.rs
@@ -15,16 +15,6 @@ fn main() {
 
     string("rust is fun!".to_owned());
 
-    // 这里，两种处理方式都行得通。
-    // `.into()` 方法会将一种类型转换为期望的类型。
-    // 如果在期望得到 `String` 类型的地方调用它，它会将 `&str` 转换为 `String` 类型。
-    string("nice weather".into());
-    // 但是如果在期望得到 `&str` 类型的地方调用它，那么 `&str` 就会保持为 `&str`，因为不需要进行转换(转了个寂寞)。
-    // 如果你移除 `#[allow(…)]` 这一行，
-    // 那么 Clippy(Rust的一个代码质量检查工具)会告诉你移除下面的 `.into()`，因为这是一个无用的转换。
-    #[allow(clippy::useless_conversion)]
-    string_slice("nice weather".into());
-
     string(format!("Interpolation {}", "Station"));
 
     // 警告: 这是字节索引(byte indexing)，而非字符索引(character indexing)。
